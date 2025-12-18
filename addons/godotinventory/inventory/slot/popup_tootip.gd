9cb1bffe16b8822b1ba18cedeeebef48f01dd56f
tdbbb <tdbbb0427@gmail.com>
2025-12-18 23:18:51 +0800

    feat: add ui update function, allowing update ui via slot.

--- addons/godotinventory/inventory/slot/popup_tootip.gd
+++ addons/godotinventory/inventory/slot/popup_tootip.gd
@@ -1,12 +1,21 @@
-extends Control
+extends PanelContainer
 class_name PopupTooltip
+## Popup tooltip show up when cursor enter the slot. Display item's information including icon, name, description, quantity.
 
 @export var item_icon : TextureRect
 @export var item_name : Label
+@export var item_description : Label
 @export var item_quantity : Label
 
+func _ready() -> void:
+	hide()
 
-func initialize(_item_icon : Texture, _item_name : String, _item_quantity : int) -> void:
-	item_icon.texture = _item_icon
-	item_name.text = _item_name
-	item_quantity.text = str(_item_quantity)
+## Update tooltip's ui of texture, name, description and quantity.
+func update_tooltip(item : Item, quantity : int) -> void:
+	if not item:
+		return
+	item_icon.texture = item.icon
+	item_name.text = item.name
+	item_description.text = item.description
+	item_quantity.text = "x%d" %quantity
+	reset_size()
